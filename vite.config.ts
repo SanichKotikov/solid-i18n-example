d22b6377c986887d1a1d3810426c601e0ca6331d
Sanich <sancho@cdrpro.ru>
2023-04-16 19:32:55 +0300

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/solid-i18n-example/',
   plugins: [solidPlugin()],
   server: {
     port: 3000,
